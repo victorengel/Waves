64149e0fb2f22195084572d312d2bf8f3a694749
Alexey Kiselev <alexey.kiselev@gmail.com>
2017-11-27 12:52:09 +0300

    Conflicts were resolved

--- src/main/scala/com/wavesplatform/matcher/MatcherSettings.scala
+++ src/main/scala/com/wavesplatform/matcher/MatcherSettings.scala
@@ -24,7 +24,6 @@ case class MatcherSettings(enable: Boolean,
                            maxTimestampDiff: FiniteDuration,
                            blacklistedAssets: Set[String],
                            blacklistedNames: Seq[Regex],
-                           txHistoryFile: Option[File],
                            restOrderLimit: Int,
                           )
 
@@ -56,6 +55,6 @@ object MatcherSettings {
 
     MatcherSettings(enabled, account, bindAddress, port, minOrderFee, orderMatchTxFee, journalDirectory,
       snapshotsDirectory, snapshotsInterval, orderCleanupInterval, maxOpenOrders, baseAssets, basePairs, maxTimestampDiff,
-      orderHistoryFile, isMigrateToNewOrderHistoryStorage, blacklistedAssets.toSet, blacklistedNames, txHistoryFile, restOrderLimit)
+      blacklistedAssets.toSet, blacklistedNames, restOrderLimit)
   }
 }
